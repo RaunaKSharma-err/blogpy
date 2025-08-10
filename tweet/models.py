a65382a0611b4623dbde9414af70771b6d547db5
RaunaKSharma-err <progritik@gmail.com>
2025-08-10 21:04:59 +0545

    tweet

--- tweet/models.py
+++ tweet/models.py
@@ -1,5 +1,15 @@
 from django.db import models
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 # Create your models here.
-def hello(request):
-    return HttpResponse("hello.......")
+
+
+class tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField(upload_to="photos/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.text[:10]}"
--- tweet/views.py
+++ tweet/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import TweetForm
+from .models import tweet
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 
 def hello(request):
     return render(request, "index.html")
+
+
+def tweetList(request):
+    tweets = tweet.objects.all().order_by("-created_at")
+    return render(request, "tweetList.html", {"tweet": tweets})
+
+
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect("tweetList")
+    else:
+        form = TweetForm()
+        return render(request, "tweet_form.html", {"form": form})
